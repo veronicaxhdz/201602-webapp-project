762ea8854ff106948d2c8a9c3aeccc8d99f27d2f
laurakoelelr <koeller@oxy.edu>
2016-04-27 09:16:49 -0700

    Updated search functions

--- my_course_counts.py
+++ my_course_counts.py
@@ -30,96 +30,100 @@ class Course_Directory:
         results = []
         for course in self.courses_list:
             match = True
-        for instructor in instructors:  # ASK JUSTIN ___.instructors
-            if not str_contains(course.instructors, instructor):
-                match = False
-                break
+            for instructor in instructors:  # ASK JUSTIN ___.instructors
+                if not str_contains(course.instructors, instructor):
+                    match = False
+                    break
             if match:
                 results.append(course)
-            return results
+        return results
 
     def search_by_title(self, title):
         results = []
         for course in self.courses_list:
             match = True
-            for title in course.title:
-                if not str_contains(title, string):
+            for word in course.title:
+                if not str_contains(course.title, word):
                     match = False
                     break
                 if match:
                     results.append(course)
                 return results
+                # veronica
 
     def search_by_season(self, season):
         results = []
         for course in self.courses_list:
             match = True
-            for course in course.season:
-                if not str_contains(season, string):
-                    match = False
-                    break
-                if match:
-                    results.append(course)
-                return results
+            if not str_contains(course.season, season):
+                match = False
+                break
+            if match:
+                results.append(course)
+            return results
+            # Veronica
 
     def search_by_department(self, department):
         results = []
         for course in self.courses_list:
             match = True
-            for department in course.department:
-                if not str_contains(department, string):
-                    match = False
-                    break
-                if match:
-                    results.append(course)
-                return results
+            if not str_contains(course.department, department):
+                match = False
+                break
+            if match:
+                results.append(course)
+            return results
+            # Veronica
 
     def search_by_core(self, core):
         results = []
         for course in self.courses_list:
             match = True
-            for core in course.core:
-                if not str_contains(core, string):
-                    match = False
-                    break
-                if match:
-                    results.append(course)
-                return results
+            if not str_contains(course.core, core):
+                match = False
+                break
+        if match:
+            results.append(course)
+        return results
 
     def search_by_year(self, year):
         results = []
         for course in self.courses_list:
             match = True
-            for year in course.year:
-                if not str_contains(year, string):
-                    match = True
-                    break
-                if match:
-                    results.append(course)
-                return results
+            if not str_contains(course.year, year):
+                match = False
+            break
+            if match:
+                results.append(course)
+            return results
 
     def search_by_number(self, number):
         results = []
         for course in self.courses_list:
             match = True
-            for number in course.number:
-                if not str_contains(number, string):
-                    match = True
-                    break
-                if match:
-                    results.append(course)
-                return results
+            if not str_contains(course.number, number):
+                match = True
+            break
+            if match:
+                results.append(course)
+            return results
 
 
 def get_data():
-    course_directory = Course_Directory()  # ASK JUSTIN
+    course_directory = Course_Directory()
+    instructor_list = []
+    title_list = []
     with open('counts.tsv') as fd:
         for line in fd.read().splitlines():
             temp_list = line.split('\t')
-            course = Course(temp_list[0], temp_list[1], temp_list[2], temp_list[3], temp_list[4], temp_list[5],
-                            temp_list[6], temp_list[7], temp_list[8], temp_list[9], temp_list[10], temp_list[11],
+            course = Course(temp_list[0], temp_list[1], temp_list[2], temp_list[3], temp_list[4], title_list,
+                            temp_list[6], instructor_list, temp_list[8], temp_list[9], temp_list[10], temp_list[11],
                             temp_list[12], temp_list[13], temp_list[14])
-            course_directory.courses_list.append(course)
+            for instructor in temp_list[7]:
+                instructor_list.append(instructor)
+            for word in temp_list[5]:
+                title_list.append(word)
+        course_directory.courses_list.append(course)
     return course_directory
 
 
